ece1eb6e34efca9e71a032122d0d0dec54975546
Dani <dannysotalin@gmail.com>
2026-05-29 01:49:05 +0200

    Update settings ssr and allowedHosts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,22 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'productos/editar/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'clientes/editar/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'usuarios/editar/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'ventas/:id',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
